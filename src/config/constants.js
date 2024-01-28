4ef06fedbca7bca96961486553127380b771aa7d
Luis Perugachi <luisprg05@gmail.com>
2024-01-28 12:56:09 -0500

    Allowed see profile pictures without autentication

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,5 @@
 export const secretKey = 'UTiNder_hard_key_#%&134';
 
-export const allowedPaths = ['/', '/auth/register', '/auth/login', '/auth/users', '/image/profile'];
+export const allowedPaths = ['/', '/auth/register', '/auth/login', '/auth/users'];
+
+export const specialPaths = { profile: '/image/profile', post: '/image/post' };
--- src/image/routes/imagesRoutes.js
+++ src/image/routes/imagesRoutes.js
@@ -5,10 +5,16 @@ import * as imageHandlers from '../handlers/index.js';
 const router = Router();
 const upload = multer();
 
-router.route('/profile/:userId').post(imageHandlers.getProfileImage);
+router.route('/profile/:userId').get(imageHandlers.getProfileImage);
 
 router.route('/profile/upload').post(upload.single('profile'), imageHandlers.uploadProfileImage);
 
 router.route('/profile/remove').delete(imageHandlers.removeProfileImage);
+
+router.route('/post/:postId').post(imageHandlers.getProfileImage);
+
+router.route('/post/upload').post(upload.single('post'), imageHandlers.uploadProfileImage);
+
+router.route('/post/remove').delete(imageHandlers.removeProfileImage);
 
 export default router;
