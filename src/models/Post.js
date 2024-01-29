018a550b0a457278a47a20e9a92dd1ca482246b5
Luis Perugachi <luisprg05@gmail.com>
2024-01-28 19:01:23 -0500

    Removed profileLink and imageUrl from db

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,7 +1,6 @@
 export const Post = (sequelize, DataTypes) => {
 	const Post = sequelize.define('posts', {
 		title: DataTypes.STRING,
-		imageUrl: DataTypes.STRING,
 		description: DataTypes.STRING,
 		likes: DataTypes.INTEGER,
 	});
